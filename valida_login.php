339bea4000c35cbaa96cc13ca0dcb70fabd43b6d
Cosmo Matias <ducosmotdl@gmail.com>
2023-01-26 11:42:52 -0300

    Aplicando controle de perfil de usuarios

--- valida_login.php
+++ valida_login.php
@@ -4,11 +4,17 @@ session_start();
 
 //variavel que verifica se a autenticacao foi realizada
 $usuario_autenticado = false;
+$usuario_id = null;
+$usuario_perfil_id = null;
+
+$perfis = array(1 => 'Admminstrativo', 2 => 'Usuário');
 
 //usuarios do sistema
 $usuarios_app = array(
-    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123', 'perfil_id' => 1),
+    array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '123', 'perfil_id' => 1),
+    array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '123', 'perfil_id' => 2),
+    array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '123', 'perfil_id' => 2),
 );
 
 /*
@@ -21,12 +27,16 @@ foreach($usuarios_app as $user) {
 
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
         $usuario_autenticado = true;
+        $usuario_id = $user['id'];
+        $usuario_perfil_id = $user['perfil_id'];
     }
 }
 
 if($usuario_autenticado) {
     echo 'Usuário autenticado';
     $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['id'] = $usuario_id;
+    $_SESSION['perfil_id'] = $usuario_perfil_id;
     header('Location: home.php');
 } else {
     $_SESSION['autenticado'] = 'NAO';
